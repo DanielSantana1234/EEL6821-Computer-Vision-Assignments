15d010d608d974bb8ee22d3ebba2deb0dab1b79c
DanielSantana1234 <danielsantana01189@gmail.com>
2026-02-09 21:24:58 -0500

    Assignment 1 continuation

--- Assignment 1 - Affine Transformations.py
+++ Assignment 1 - Affine Transformations.py
@@ -1,6 +1,14 @@
 from PIL import Image
 
-image_path = "Images/Assignment 1 Images/"
-img = Image.open(image_path)
-width, height = img.size
+image_path1 = "Images/Assignment 1 Images/Street.bmp"
+image_path2 = "Images/Assignment 1 Images/Black Grid.bmp"
 
+img1 = Image.open(image_path1)
+img2 = Image.open(image_path2)
+
+width1, height1 = img1.size
+width2, height2 = img2.size
+
+img1.paste(img2, (int(width1/2) - 300, int(height1/2) + 700), mask = img2)
+
+img1.show()
