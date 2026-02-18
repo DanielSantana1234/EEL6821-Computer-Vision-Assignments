60029c650bcb8637dc1e640a73ccd100c683d712
DanielSantana1234 <danielsantana01189@gmail.com>
2026-02-18 01:48:05 -0500

    Assignment 1 finish with projection

--- Assignment 1 - Affine Transformations.py
+++ Assignment 1 - Affine Transformations.py
@@ -1,19 +1,25 @@
 from PIL import Image
 import numpy as np
+import matplotlib.pyplot as plt
 
 image_path1 = "Images/Assignment 1 Images/Street.bmp"
-image_path2 = "Images/Assignment 1 Images/Black Grid.bmp"
+image_2 = np.zeros((200, 200))
+
+spacing = 20
+
+image_2[::spacing, :] = 1
+image_2[:, ::spacing] = 1
 
 img1 = Image.open(image_path1)
 gray_img1 = img1.convert("L")
-img2 = Image.open(image_path2)
+
+image_2 = Image.fromarray((image_2 * 255).astype(np.uint8))
 
 width1, height1 = gray_img1.size
 
-res2 = img2.resize((1600, 1600))
-width2, height2 = res2.size
+width2, height2 = image_2.size
 
-gray_img1.paste(res2, (int(width1/2) - 750, int(height1/2) + 100), mask = res2)
+#gray_img1.paste(image_2, (int(width1/2) - 750, int(height1/2) + 100), mask = image_2)
 
 def projection_result(img1: Image, img2: Image) -> Image:
     """
@@ -37,29 +43,34 @@ def warp_perspective(img1: Image, img2: Image) -> None:
     projection_matrix = np.array([
         [1, 0, 0, 0],
         [0, 1, 0, 0],
-        [0, 0, 0, 0],
-        [0, 0, 1/500, 1]
+        [0, 0, 1, 0],
+        [0, 0, -1/500, 1]
     ])
     # warped_image = Image.new("L", (width1, height1))
     for x in range(width2):
         for y in range(height2):
-            for z in range(10):
-                pixel_coord = np.array([x, y, 0, z/0.03 + 1])
-                result = np.dot(projection_matrix, pixel_coord)
+            curr_coord = img2.getpixel((x, y))
+            if curr_coord == 0:
+                continue
 
-                # Normalize the result
-                result[0] /= result[3]
-                result[1] /= result[3]
-                result[2] /= result[3]
+            z = (y - height2 / 2.0) * 4.0
+            
+            pixel_coord = np.array([x, y, z, 1])
+            result = np.dot(projection_matrix, pixel_coord)
 
-                # Map the result back to the image coordinates
-                x_new = int(result[0])
-                y_new = int(result[1])
-                z_new = int(result[2])
+            # Normalize the result
+            result[0] /= result[3]
+            result[1] /= result[3]
+            result[2] /= result[3]
 
-                # Set the pixel value in the warped image
-                if 0 <= x_new < width1 and 0 <= y_new < height1 and 0 <= z_new < 10:
-                    img1.putpixel((x_new, y_new), img2.getpixel((x, y)))
+            # Map the result back to the image coordinates
+            x_new = int(result[0])
+            y_new = int(result[1])
+            z_new = int(result[2])
+
+            # Set the pixel value in the warped image
+            if 0 <= x_new < width1 and 0 <= y_new < height1:
+                img1.putpixel((x_new, y_new), img2.getpixel((x, y)))
                 
     img1.show()
 
@@ -67,4 +78,4 @@ def warp_perspective(img1: Image, img2: Image) -> None:
 
 # warp_perspective(gray_img1, img2)
 # gray_img1.show()
-warp_perspective(gray_img1, res2)
+warp_perspective(gray_img1, image_2)
